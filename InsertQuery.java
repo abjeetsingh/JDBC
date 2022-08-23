62a408db51729418bbf3eaa46dbc3825b99898c6
Abjeet Singh <abjeets50@gmail.com>
2022-08-24 01:13:59 +0530

    feat: dynamic insert

--- InsertQuery.java
+++ InsertQuery.java
@@ -1,3 +1,6 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.*;
 public class InsertQuery {
     public static void main(String[] args){
@@ -7,12 +10,23 @@ public class InsertQuery {
             if(con.isClosed()) System.out.println("Connection not established");
             else {
                 System.out.println("Connection Established");
-                String q = "Insert into school values (?,?)";
+                String q = "Insert into Ljdbc(Name,City) values (?,?)";
                 PreparedStatement stmt = con.prepareStatement(q);
-                stmt.setString(1,"Abjeet");
-                stmt.setString(2,"Delhi");
-                stmt.executeUpdate();
-                System.out.println("Inserted");
+                // making the input Dynamic
+                String check = "y";
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                do {
+                    System.out.print("\nEnter the Name:\t");
+                    String name = br.readLine();
+                    System.out.print("Enter the City:\t");
+                    String city = br.readLine();
+                    stmt.setString(1, name);
+                    stmt.setString(2, city);
+                    stmt.executeUpdate();
+                    System.out.println("\nInserted");
+                    System.out.print("Do you want to enter more values?(Y/N)-->");
+                    check = br.readLine();
+                }while(check.toLowerCase().equals("y"));
                 con.close();
             }
         }catch(Exception e){
